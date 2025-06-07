99b29809b395f70429d4ac28fbe85387afc223c5
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-06-07 04:42:01 +0100

    feat: set up CORS and body-parser middleware for API

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+// parse form data
+app.use(express.json());
+
+// CORS
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 // Routes
 app.use(require("./routes"));
