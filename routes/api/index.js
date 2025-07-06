b55c7335f005f707c3ac1604dba89b6e1c4b5b06
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-07-06 23:04:08 +0100

    feat: implement authentication and role-based authorization for user routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,16 @@
 const router = require("express").Router();
+const { authenticate, authorizeRole } = require("../../middlewares/auth");
 
 router.use(require("../../middlewares/loginVerifire"));
 router.use("/auth", require("./auth"));
 router.use("/root", require("./root"));
 router.use("/pages", require("./pages"));
-router.use("/user", require("./user"));
+router.use(
+  "/user",
+  authenticate,
+  authorizeRole("user", "admin"),
+  require("./user")
+);
 router.use("/admin", require("./admin"));
 
 router.use((req, res) => {
