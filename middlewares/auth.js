09c20ae2ba151b7627d51cae62547eb81e8c1717
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-07-06 23:04:23 +0100

    refactor: enhance authentication middleware to include action messages in responses

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,30 +1,39 @@
 const jwt = require("jsonwebtoken");
+const getUser = require("../utils/getUser");
 
-export function authenticate(req, res, next) {
+async function authenticate(req, res, next) {
   const token =
     req.cookies?.userToken || req.headers.authorization?.split(" ")[1];
 
   if (!token) {
-    return res.status(404).json({ status: "error", message: "No token" });
+    return res
+      .status(404)
+      .json({ status: "error", message: "No token", action: "logout" });
   }
 
   try {
     const decodedToken = jwt.verify(token, process.env.USER_TOKEN_SECRET);
-    req.user = decodedToken;
+    req.user = decodedToken?.user;
     next();
   } catch (error) {
     console.log(error);
-    res.status(401).json({ status: "error", message: "Invalide token" });
+    res
+      .status(401)
+      .json({ status: "error", message: "Invalide token", action: "logout" });
   }
 }
 
-export function authorizeRole(...allowedRoles) {
+function authorizeRole(...allowedRoles) {
   return (req, res, next) => {
     if (!req.user || !allowedRoles.includes(req.user.role)) {
-      return res
-        .status(403)
-        .json({ status: "error", message: "Access denied" });
+      return res.status(403).json({
+        status: "error",
+        message: "Access denied",
+        action: "accessDenied",
+      });
     }
     next();
   };
 }
+
+module.exports = { authenticate, authorizeRole };
