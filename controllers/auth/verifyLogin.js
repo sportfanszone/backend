889884ef2ea406f974e724229e24c1e4cb0d3e33
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-07-08 19:06:40 +0100

    refactor: remove unnecessary console logs

--- controllers/auth/verifyLogin.js
+++ controllers/auth/verifyLogin.js
@@ -1,6 +1,4 @@
 module.exports = (req, res) => {
-  console.log("Verifying login status...");
-  console.log(req.cookies);
   const token = req.cookies.userToken;
   if (!token) return res.status(401).json({ loggedIn: false });
 
