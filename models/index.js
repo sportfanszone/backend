c7efda17943cd3d2e91314b0009cb9f9a3632fb1
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-07-21 02:46:39 +0100

    feat: add Post and PostFile models with associations for club and user posts

--- models/index.js
+++ models/index.js
@@ -6,6 +6,8 @@ const League = require("./League");
 const Club = require("./Club");
 const ActivityLog = require("./ActivityLog");
 const Follow = require("./Follow");
+const Post = require("./Post");
+const PostFile = require("./PostFile");
 
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
@@ -32,6 +34,8 @@ db.League = League(sequelize, DataTypes);
 db.Club = Club(sequelize, DataTypes);
 db.ActivityLog = ActivityLog(sequelize, DataTypes);
 db.Follow = Follow(sequelize, DataTypes);
+db.Post = Post(sequelize, DataTypes);
+db.PostFile = PostFile(sequelize, DataTypes);
 
 // Associations
 db.User.belongsTo(db.Club);
@@ -56,5 +60,22 @@ db.User.belongsToMany(db.User, {
   foreignKey: "followingId",
   otherKey: "followerId",
 });
+
+db.User.hasMany(db.Post, {
+  foreignKey: "UserId",
+  as: "posts",
+});
+db.Post.belongsTo(db.Club, {
+  foreignKey: "ClubId",
+  as: "club",
+});
+db.Club.hasMany(db.Post, {
+  foreignKey: "ClubId",
+  as: "posts",
+});
+db.Post.hasMany(db.PostFile, {
+  onDelete: "CASCADE",
+});
+db.PostFile.belongsTo(db.Post);
 
 module.exports = db;
