a5cdf8554f091c3f48cd093b9a65102e5e0f265b
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-07-22 22:45:32 +0100

    feat: add functionality to upload profile photo and cover photo while creating user from admin panel

--- controllers/admin/addUser.js
+++ controllers/admin/addUser.js
@@ -1,14 +1,20 @@
+const path = require("path");
 const { User } = require("../../models");
+const { uploadAsync } = require("../../middlewares/userUpload");
+const deleteUploadedFiles = require("../../utils/deleteUploadedFiles");
 
 module.exports = async (req, res) => {
   try {
-    console.log("req.body");
-    console.log(req.body);
+    const filePath = path.join(process.cwd(), "public", "images", "user");
+
+    await uploadAsync(req, res);
+
     // Check if email already is in use
     const emailExists = await User.findOne({
       where: { email: req.body.email },
     });
     if (emailExists) {
+      deleteUploadedFiles(req.files, filePath);
       return res.status(400).json({
         status: "error",
         message: "Email already in use",
@@ -21,6 +27,7 @@ module.exports = async (req, res) => {
       where: { username: req.body.username },
     });
     if (usernameExsits) {
+      deleteUploadedFiles(req.files, filePath);
       return res.status(400).json({
         status: "error",
         message: "Username already in use",
@@ -28,9 +35,20 @@ module.exports = async (req, res) => {
       });
     }
 
+    const profilePhoto = req.files.profilePhoto
+      ? `${process.env.BACKEND_URL}/images/user/${req.files.profilePhoto[0].filename}`
+      : null;
+    const coverPhoto = req.files.coverPhoto
+      ? `${process.env.BACKEND_URL}/images/user/${req.files.coverPhoto[0].filename}`
+      : null;
+
+    req.body.profileImageUrl = profilePhoto;
+    req.body.coverPhotoUrl = coverPhoto;
+
     //   Create user
     const userCreated = await require("../../utils/createUser")(req.body);
     if (!userCreated) {
+      deleteUploadedFiles(req.files, filePath);
       return res.status(500).json({
         status: "error",
         message: "Failed to create user",
@@ -44,6 +62,23 @@ module.exports = async (req, res) => {
     });
   } catch (error) {
     console.error("Error in signup:", error);
+
+    deleteUploadedFiles(req.files, filePath);
+
+    if (error instanceof multer.MulterError) {
+      if (error.code === "LIMIT_FILE_SIZE") {
+        return res.status(400).json({
+          status: "error",
+          message: "File size too large. Maximum file size is 2MB",
+        });
+      } else {
+        return res.status(400).json({
+          status: "error",
+          message: "Failed to upload file",
+        });
+      }
+    }
+
     return res.status(500).json({
       status: "error",
       message: "An unexpected error occurred",
