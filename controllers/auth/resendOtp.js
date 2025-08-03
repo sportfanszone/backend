33e77a9923c8d9d4076d6545109f416d8e81f30f
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-08-03 08:37:12 +0100

    feat: send OTP via email on resendOtp request
    - Integrated OTP email sending in the resendOtp route
    - Used `signupOtp.mail` utility to send OTP to user's email
    - Wrapped logic in try-catch to handle errors gracefully

--- controllers/auth/resendOtp.js
+++ controllers/auth/resendOtp.js
@@ -1,16 +1,31 @@
-module.exports = (req, res) => {
-  const newOtp = require("../../utils/genOtp");
-  req.session.otp = newOtp();
-  req.session.otpExpires = Date.now() + 3 * 60 * 1000;
-  console.log("Resent OTP:", req.session.otp);
-  res.cookie("allowOtp", "true", {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    maxAge: 3 * 60 * 1000,
-    sameSite: "lax",
-  });
-  res.json({
-    status: "success",
-    message: "OTP resent successfully",
-  });
+module.exports = async (req, res) => {
+  try {
+    const newOtp = require("../../utils/genOtp")();
+    req.session.otp = newOtp;
+    req.session.otpExpires = Date.now() + 3 * 60 * 1000;
+
+    console.log("Resent OTP:", req.session.otp);
+
+    await require("../../utils/mails/signupOtp.mail")({
+      user: req.session.user,
+      otpCode: newOtp,
+    });
+
+    res.cookie("allowOtp", "true", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 3 * 60 * 1000,
+      sameSite: "lax",
+    });
+    res.json({
+      status: "success",
+      message: "OTP resent successfully",
+    });
+  } catch (error) {
+    console.error("Error in signup:", error);
+    return res.status(500).json({
+      status: "error",
+      message: "An unexpected error occurred",
+    });
+  }
 };
