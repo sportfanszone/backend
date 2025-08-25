fc8e6fd8a7fd0b129f07abce9dff02b4823d4002
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-08-25 20:54:44 +0100

    chore: remove debug console.log from getComment controller

--- controllers/user/getComment.js
+++ controllers/user/getComment.js
@@ -13,8 +13,6 @@ module.exports = async (req, res) => {
       });
     }
 
-    console.log("---Reached here");
-
     const comments = await require("../../utils/getCommentsByPostId")(
       postId,
       req.user.id
